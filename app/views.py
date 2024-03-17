373c057e4bf9503d4b30574cb2283b65bebb0826
Josiah-John Green <josiahjohngreen@gmail.com>
2024-03-16 19:53:49 -0500

    Completed Property Listing

--- app/views.py
+++ app/views.py
@@ -6,10 +6,12 @@ This file contains the routes for your application.
 """
 
 import os
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
+from werkzeug.utils import secure_filename
 from app import app, db
 from app.forms import PropertyForm
 from app.models import Property
+from app.utils import get_uploaded_images
 
 ###
 # Routing for your application.
@@ -28,15 +30,20 @@ def about():
 
 @app.route('/properties')
 def properties():
-    """Render the website's properties page."""
-    return render_template('properties.html')
+    """Render the website's propery listing page."""
+    properties = Property.query.all()
+    return render_template('properties.html', properties=properties)
 
 @app.route('/properties/create', methods=['GET', 'POST'])
 def create():
-
+    """Render the website's property creation page."""
     form = PropertyForm()
 
     if form.validate_on_submit():
+        photo = form.photo.data  # Access the uploaded file
+        filename = secure_filename(photo.filename)  # Secure the filename
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # Save the photo to the designated folder
+
         property = Property(
             title=form.title.data,
             description=form.description.data,
@@ -45,7 +52,7 @@ def create():
             price=form.price.data,
             type=form.type.data,
             location=form.location.data,
-            photo=form.photo.data.filename  # Assuming photo is uploaded using Flask-WTF FileField
+            photo=filename  # Store the filename in the database
         )
         db.session.add(property)
         db.session.commit()
@@ -57,6 +64,16 @@ def create():
 
     return render_template('create.html', form=form)
 
+@app.route('/uploads/<filename>')
+def photo(filename):
+    """Render the page for a specific property."""
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+
+@app.route('/properties/<int:propertyid>')
+def view(propertyid):
+    """Render the page for a specific property."""
+    property = Property.query.get_or_404(propertyid)
+    return render_template('view.html', property=property)
 
 ###
 # The functions below should be applicable to all Flask apps.
