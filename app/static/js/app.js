04bc81fa1fd6ea6d425a098f562537d4e18d364a
Josiah-John Green <josiahjohngreen@gmail.com>
2024-03-17 23:23:09 -0500

    Fixed Flash Message Issues

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   
-  //Script: Files
+  /* Script: Flash Exit */
+
+  // Get the close button for the flash message
+  const closeButton = document.getElementById('close-flash');
+
+  // Add click event listener to the close button
+  closeButton.addEventListener('click', function() {
+      // Find the flash message div and hide it
+      const flashMessage = document.getElementById('flash-msg');
+      flashMessage.style.display = 'none';
+  });
+
+  /* Script: Files */
 
   document.getElementById('file-upload').addEventListener('change', function() {
 
@@ -12,7 +24,5 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('selected-file').innerHTML = fileName;
   
   });
-
-
   
 }); 
